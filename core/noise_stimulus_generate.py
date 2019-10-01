7e830cc88c26157851ef35223e4bf40d50f5e4e3
Yukun Qu <532021098@qq.com>
2019-10-01 17:21:27 +0800

    ‘add function to generate sinusoid noise’

--- core/noise_stimulus_generate.py
+++ core/noise_stimulus_generate.py
@@ -1,23 +1,63 @@
 import numpy as np
 
 
-def generateSingleSinusoid(img_size, cycles, angle, phase):
+def generateSingleSinusoid(img_size, angle, phase):
     angle = np.radians(angle)
-    sinepath = (np.linspace(0,cycles,img_size))[:,np.newaxis].repeat(img_size,axis=1)
+    sinepath = (np.linspace(0,2,img_size))[:,np.newaxis].repeat(img_size,axis=1)
     sinusoid = (sinepath*np.sin(angle) + sinepath.T*np.cos(angle)) * 2 *np.pi
     sinusoid = np.sin(sinusoid + phase)
+
     return sinusoid
 
 
-def generateCombinSinusoid(img_size,cycles,angle,phase):
+def generatePatch(img_size):
+    angle = [0,30,60,90,120,150]
+    phases = [0,np.pi/2]
     sinusoid_combined = []
-    for p in phase:
+
+    for p in phases:
         for a in angle:
-            sinusoid = generateSingleSinusoid(img_size,cycles,a,p)
-            sinusoid = np.random.randn() * sinusoid
+            sinusoid = generateSingleSinusoid(img_size,a,p)
             sinusoid_combined.append(sinusoid)
-    sinusoid_combined = np.array(sinusoid_combined)
-    sinusoid_combined = np.sum(sinusoid_combined,axis=0)
-    sinusoid_combined = (sinusoid_combined-sinusoid_combined.min())/(sinusoid_combined.max()-sinusoid_combined.min())
-    sinusoid_combined = sinusoid_combined *255
+    sinusoid_combined = np.array(sinusoid_combined)    #actually need to multiply factors
+    #sinusoid_combined = np.sum(sinusoid_combined,axis=0)# for test, no need to sum()
     return sinusoid_combined
+
+
+def generatePatches(img_size,nscale=5):
+    scales = [2**i for i in range(1,nscale+1)]
+    NumPatch = [(scale/2)**2 for scale in scales]
+    patches = {}
+
+    for scale,numpatch in zip(scales,NumPatch):
+        img_size = img_size / (scale/2)
+        patch = generatePatch(img_size)
+        patch = np.tile(patch,(1,(scale/2),(scale/2)))
+        patches[scale] = patch
+
+    return patches
+
+
+def generateNoise(img_size,patches,nscale=5):
+
+    scales = [2**i for i in range(1,nscale+1)]
+    NumPatch = [(scale / 2) ** 2 for scale in scales]
+    NumParam = [ num*12 for num in NumPatch]
+    params = {}
+
+    for scale,numpatch,numpara in zip(scales,NumPatch,NumParam):
+
+        img_size = img_size / (scale/2)
+        patch = generatePatch(img_size)
+        patch = np.tile(patch,(1,(scale/2),(scale/2)))
+        patches[scale] = patch
+
+        param = np.random.randn(numpara)
+        param = param.reshape(12,numpatch)
+        param = param.repeat(img_size,axis=0).repeat(img_size,axis=1)
+        params[scale] = param
+
+    noise = [patches[scale] * params[scale] for scale in scales]
+    noise = np.sum(np.array(noise),axis=0)
+
+    return noise,params
